0b5f8735c80173828398ed0794ac8640968abd3c
Devin Harris <hello@devinhrrs.me>
2025-01-02 19:35:14 -0500

    updates

---
+++ app/products/page.tsx
@@ -0,0 +1,10 @@
+import styles from './ProductPage.module.scss'
+
+export default function ProductsPage() {
+
+    return (
+        <div className={styles.productPage}>
+            <h1>Product Page</h1>
+        </div>
+    )
+}
